8b25e1536d5ce212d7ffcdf5860274c533e07b7c
kai <ki@qq.com>
2023-02-02 14:33:41 +0800

    second commit

--- src/main/java/git/test/GitTest.java
+++ src/main/java/git/test/GitTest.java
@@ -3,5 +3,6 @@ package git.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,world!");
+        System.out.println("hello git");
     }
 }
